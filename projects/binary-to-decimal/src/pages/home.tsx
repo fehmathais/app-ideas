e606fbf62bb8bfba27b79ba32c7954781c0e70e8
Felipe Mathais <fmathais@gmail.com>
2020-04-23 18:39:05 -0300

    Add first project

---
+++ projects/binary-to-decimal/src/pages/home.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import styles from'./index.module.scss';
+
+export default class Home extends Component<any, any> {
+	render() {
+		return (
+			<div className={styles.app}>
+				<header className={styles.header}>
+					<img src={logo} className={styles.logo} alt="logo" />
+					<p>
+						Edit <code>src/App.tsx</code> and save to reload.
+					</p>
+
+					<a className={styles.link} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+						Learn React
+					</a>
+				</header>
+			</div>
+		);
+	}
+};
