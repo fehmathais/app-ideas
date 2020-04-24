37875a2b5099e5d158b9a0b3c6db041bd775987c
Felipe Mathais <fmathais@gmail.com>
2020-04-24 11:09:16 -0300

    Add binary to decimal converter.

--- projects/binary-to-decimal/src/pages/home.tsx
+++ projects/binary-to-decimal/src/pages/home.tsx
@@ -1,21 +1,59 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
+import React, { Component, ChangeEvent, KeyboardEvent } from 'react';
 import styles from'./index.module.scss';
 
-export default class Home extends Component<any, any> {
+interface IState {
+	binary: string,
+	decimal: number
+}
+
+export default class Home extends Component<any, IState> {
+	state: IState = {
+		binary: '',
+		decimal: 0
+	}
+
+	handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		if (this.state.binary.length <= 7) {
+			this.setState({binary: event.target.value});
+		}
+	}
+
+	handleKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
+		if (event.key === 'Enter') {
+			this.generateDecimal();
+		}
+	}
+
+	generateDecimal = () => {
+		const revertedBinary = this.state.binary.split('').reverse().join('');
+
+		let total = 0;
+
+		this.setState({decimal: 0}, () => {
+			for (let i = 0; i < revertedBinary.length; i++) {
+				const binaryDigit: number = parseInt(revertedBinary[i]);
+				total += (Math.pow(2, i) * binaryDigit);
+			}
+
+			this.setState({decimal: total});
+		});
+	}
+
 	render() {
 		return (
 			<div className={styles.app}>
-				<header className={styles.header}>
-					<img src={logo} className={styles.logo} alt="logo" />
-					<p>
-						Edit <code>src/App.tsx</code> and save to reload.
-					</p>
+				<div className={styles.container}>
+					<h1>Binary to decimal converter</h1>
 
-					<a className={styles.link} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-						Learn React
-					</a>
-				</header>
+					<input type="tel"
+						   maxLength={8}
+						   value={this.state.binary}
+						   onChange={this.handleChange}
+						   onKeyPress={this.handleKeyPress}/>
+					<button onClick={this.generateDecimal}>Convert</button>
+
+					<p>Converted: {this.state.decimal}</p>
+				</div>
 			</div>
 		);
 	}
